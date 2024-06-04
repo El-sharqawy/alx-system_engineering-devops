3498f81924599c542632c1abf50177938893568b
El-sharqawy <elsharqawy2@gmail.com>
2024-06-04 18:00:59 +0300

    Advanced API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns the number of subscribers"""
+"""
+queries the Reddit API and returns the number of subscribers
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """queries the Reddit API and returns the number of subscribers"""
+    """
+    queries the Reddit API and returns the number of subscribers
+    """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
